ec2d17e118c6a5dc5b5703a1e27477c9b2a189f9
Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>
2021-03-10 20:25:13 +0100

    tests: add mock for clock_settime
    Signed-off-by: Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>

--- tests/Tests.cmake
+++ tests/Tests.cmake
@@ -2,5 +2,9 @@ add_executable(general_utest
 	tests/general_utest.c 
 	src/utils/ntp/server_list.c
 	src/utils/memory.c)
-target_link_libraries(general_utest ${CMOCKA_LIBRARIES} ${SYSREPO_LIBRARIES} ${LIBYANG_LIBRARIES})
+target_link_libraries(general_utest
+	${CMOCKA_LIBRARIES}
+	${SYSREPO_LIBRARIES}
+	${LIBYANG_LIBRARIES}
+	"-Wl,--wrap=clock_settime")
 add_test(NAME general_utest COMMAND general_utest)
--- tests/general_utest.c
+++ tests/general_utest.c
@@ -6,20 +6,63 @@
 #define PLUGIN
 #include "general.c"
 
-static void set_datetime_test(void **state);
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+
+static void test_correct_set_datetime(void **state);
+static void test_incorrect_set_datetime(void **state);
 
 int main(void)
 {
 	const struct CMUnitTest tests[] = {
-		cmocka_unit_test(set_datetime_test),
+		cmocka_unit_test(test_correct_set_datetime),
+		cmocka_unit_test(test_incorrect_set_datetime),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
 }
 
-static void set_datetime_test(void **state)
+int __wrap_clock_settime(clockid_t clock_id, const struct timespec *tp)
+{
+	check_expected(clock_id);
+	return (int) mock();
+}
+
+
+static void test_correct_set_datetime(void **state)
 {
 	(void) state;
+	int rc = 0;
+
+	expect_value(__wrap_clock_settime, clock_id, CLOCK_REALTIME);
+	will_return(__wrap_clock_settime, 0);
+	rc = set_datetime("2021-02-09T09:02:39Z");
+	assert_int_equal(rc, 0);
 
 	return;
+}
+
+static void test_incorrect_set_datetime(void **state)
+{
+	(void) state;
+	int rc = 0;
+	struct {
+		char *datetime;
+		int expected_rc;
+	} incorrect_table[] = {
+		// Correct but currently unsupported
+		{.datetime = "2021-02-09T06:02:39.234+01:00", .expected_rc = -1},
+		{.datetime = "2021-02-09T06:02:39.234Z", .expected_rc = -1},
+		{.datetime = "2021-02-09T06:02:39+11:11", .expected_rc = -1},
+		// incorrect
+		{.datetime = "AAAA-02-09T06:02:39.234Z", .expected_rc = -1},
+		{.datetime = "210392019302193", .expected_rc = -1},
+		{.datetime = "---------", .expected_rc = -1},
+	};
+
+	for (size_t i = 0; i < ARRAY_SIZE(incorrect_table); i++) {
+		rc = set_datetime(incorrect_table[i].datetime);
+		assert_int_equal(rc, incorrect_table[i].expected_rc);
+	}
+
+
 }
