4f85c77d2e280d9618cb27deb7477960703b91fe
Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>
2021-05-09 19:06:24 +0200

    tests: load initial data in individual tests
    Signed-off-by: Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>

--- tests/integration/ietf-system.py
+++ tests/integration/ietf-system.py
@@ -6,17 +6,15 @@ class SystemTestCase(unittest.TestCase):
         self.conn = sysrepo.SysrepoConnection()
         self.session = self.conn.start_session("running")
 
-        self.load_initial_data()
-
     def tearDown(self):
         self.session.stop()
         self.conn.disconnect()
 
-    def load_initial_data(self):
+    def load_initial_data(self, path):
         ctx = self.conn.get_ly_ctx()
 
         self.session.replace_config_ly(None, "ietf-system")
-        f = open("system_contact.xml", "r")
+        f = open(path, "r")
         data = f.read()
         data = ctx.parse_data_mem(data, "xml", config=True, strict=True)
         self.session.replace_config_ly(data, "ietf-system")
@@ -24,6 +22,8 @@ class SystemTestCase(unittest.TestCase):
 
     def test_contact_get(self):
         expected_contact = '<system xmlns="urn:ietf:params:xml:ns:yang:ietf-system"><contact>test_contac</contact></system>'
+
+        self.load_initial_data("system_contact.xml")
 
         data = self.session.get_data_ly('/ietf-system:system/contact')
         contact = data.print_mem("xml")
