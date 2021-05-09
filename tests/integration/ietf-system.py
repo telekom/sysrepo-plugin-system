d921650993995f6d3df7c8a1dd3c3e7547d2b66d
Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>
2021-05-09 18:58:51 +0200

    tests: add initial integration tests
    Signed-off-by: Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>

---
+++ tests/integration/ietf-system.py
@@ -0,0 +1,34 @@
+import unittest
+import sysrepo
+
+class SystemTestCase(unittest.TestCase):
+    def setUp(self):
+        self.conn = sysrepo.SysrepoConnection()
+        self.session = self.conn.start_session("running")
+
+        self.load_initial_data()
+
+    def tearDown(self):
+        self.session.stop()
+        self.conn.disconnect()
+
+    def load_initial_data(self):
+        ctx = self.conn.get_ly_ctx()
+
+        self.session.replace_config_ly(None, "ietf-system")
+        f = open("system_contact.xml", "r")
+        data = f.read()
+        data = ctx.parse_data_mem(data, "xml", config=True, strict=True)
+        self.session.replace_config_ly(data, "ietf-system")
+        f.close()
+
+    def test_contact_get(self):
+        expected_contact = '<system xmlns="urn:ietf:params:xml:ns:yang:ietf-system"><contact>test_contac</contact></system>'
+
+        data = self.session.get_data_ly('/ietf-system:system/contact')
+        contact = data.print_mem("xml")
+        self.assertEqual(contact, expected_contact, "contact data is wrong")
+        data.free()
+
+if __name__ == '__main__':
+    unittest.main()
