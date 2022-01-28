3f78978594462324a0270a1e26ebe7a05b1f36ed
Filip Maric <filip.maric@sartura.hr>
2022-01-28 11:41:42 +0000

    general-plugin: update api calls - logs, oper sub params etc

--- src/general.h
+++ src/general.h
@@ -15,6 +15,8 @@
 #define GENERAL_PLUGIN_H
 #include <sysrepo.h>
 
+#define PLUGIN_NAME "system-plugin"
+
 int sr_plugin_init_cb(sr_session_ctx_t *session, void **private_data);
 void sr_plugin_cleanup_cb(sr_session_ctx_t *session, void *private_data);
 
