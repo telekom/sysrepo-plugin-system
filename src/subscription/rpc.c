54495ffa057e88d3c951006c8e9b4a7f9e228697
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-05-20 00:49:55 +0200

    system-plugin: change set-current-datetime RPC initial error value

--- src/subscription/rpc.c
+++ src/subscription/rpc.c
@@ -14,7 +14,7 @@ static int system_set_current_datetime(const char *datetime);
 
 int system_rpc_set_current_datetime(sr_session_ctx_t *session, uint32_t subscription_id, const char *op_path, const sr_val_t *input, const size_t input_cnt, sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt, void *private_data)
 {
-	int error = SR_ERR_CALLBACK_FAILED;
+	int error = SR_ERR_OK;
 
 	const char *current_datetime = NULL;
 
