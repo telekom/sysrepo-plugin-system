243eb10afd58bbc21d938f831c22643d9e7efbbd
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-05-19 16:01:40 +0200

    system-plugin: add set-current-datetime RPC implementation

--- src/subscription/rpc.c
+++ src/subscription/rpc.c
@@ -1,9 +1,43 @@
 #include "rpc.h"
+#include "common.h"
+
+#include <assert.h>
+#include <sysrepo.h>
+
+#include <time.h>
+
+// helpers //
+
+static int system_set_current_datetime(const char *datetime);
+
+////
 
 int system_rpc_set_current_datetime(sr_session_ctx_t *session, uint32_t subscription_id, const char *op_path, const sr_val_t *input, const size_t input_cnt, sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt, void *private_data)
 {
 	int error = SR_ERR_CALLBACK_FAILED;
 
+	const char *current_datetime = NULL;
+
+	// assert only one input value - datetime
+	assert(input_cnt == 1);
+
+	current_datetime = input[0].data.string_val;
+
+	error = system_set_current_datetime(current_datetime);
+	if (error) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "system_set_datetime() error (%d)", error);
+		goto error_out;
+	}
+
+	SRPLG_LOG_INF(PLUGIN_NAME, "System time successfully set.");
+
+	goto out;
+
+error_out:
+	error = SR_ERR_CALLBACK_FAILED;
+	SRPLG_LOG_ERR(PLUGIN_NAME, "Failed to set system time.");
+
+out:
 	return error;
 }
 
@@ -19,4 +53,28 @@ int system_rpc_shutdown(sr_session_ctx_t *session, uint32_t subscription_id, con
 	int error = SR_ERR_CALLBACK_FAILED;
 
 	return error;
+}
+
+static int system_set_current_datetime(const char *current_datetime)
+{
+	struct tm t = {0};
+	time_t time_to_set = 0;
+	struct timespec stime = {0};
+
+	if (strptime(current_datetime, "%FT%TZ", &t) == 0) {
+		return -1;
+	}
+
+	time_to_set = mktime(&t);
+	if (time_to_set == -1) {
+		return -1;
+	}
+
+	stime.tv_sec = time_to_set;
+
+	if (clock_settime(CLOCK_REALTIME, &stime) == -1) {
+		return -1;
+	}
+
+	return 0;
 }
