deacf53e947065a7a9067eb29b5cd82f399e301f
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-05-20 00:52:54 +0200

    system-plugin: implement restart and shutdown RPCs

--- src/subscription/rpc.c
+++ src/subscription/rpc.c
@@ -43,14 +43,22 @@ out:
 
 int system_rpc_restart(sr_session_ctx_t *session, uint32_t subscription_id, const char *op_path, const sr_val_t *input, const size_t input_cnt, sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt, void *private_data)
 {
-	int error = SR_ERR_CALLBACK_FAILED;
+	int error = SR_ERR_OK;
+
+	sync();
+	system("shutdown -r");
+	SRPLG_LOG_INF(PLUGIN_NAME, "Restarting the system!");
 
 	return error;
 }
 
 int system_rpc_shutdown(sr_session_ctx_t *session, uint32_t subscription_id, const char *op_path, const sr_val_t *input, const size_t input_cnt, sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt, void *private_data)
 {
-	int error = SR_ERR_CALLBACK_FAILED;
+	int error = SR_ERR_OK;
+
+	sync();
+	system("shutdown -P");
+	SRPLG_LOG_INF(PLUGIN_NAME, "Shutting down the system!");
 
 	return error;
 }
