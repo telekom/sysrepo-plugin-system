e0d1ea2b5553fdcfa9874efe83d8b6768aef184d
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-05-24 20:28:09 +0200

    system-plugin: add authentication store API

---
+++ src/system/authentication/api/store.c
@@ -0,0 +1,15 @@
+#include "store.h"
+
+int system_authentication_store_user(system_ctx_t *ctx, UT_array *arr)
+{
+	int error = 0;
+
+	return error;
+}
+
+int system_authentication_store_user_authorized_key(system_ctx_t *ctx, const char *user, UT_array *arr)
+{
+	int error = 0;
+
+	return error;
+}
--- src/system/authentication/api/store.h
+++ src/system/authentication/api/store.h
@@ -1,4 +1,10 @@
 #ifndef SYSTEM_PLUGIN_AUTHENTICATION_API_STORE_H
 #define SYSTEM_PLUGIN_AUTHENTICATION_API_STORE_H
 
+#include "context.h"
+#include <utarray.h>
+
+int system_authentication_store_user(system_ctx_t *ctx, UT_array *arr);
+int system_authentication_store_user_authorized_key(system_ctx_t *ctx, const char *user, UT_array *arr);
+
 #endif // SYSTEM_PLUGIN_AUTHENTICATION_API_STORE_H
