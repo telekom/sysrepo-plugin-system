92dbe7d25bbd9d5d9386ddf2dd7006715d2fa1fc
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-05-24 15:22:21 +0200

    system-plugin: change context typedef

--- src/context.h
+++ src/context.h
@@ -3,8 +3,10 @@
 
 #include <sysrepo_types.h>
 
-typedef struct system_ctx_s {
+typedef struct system_ctx_s system_ctx_t;
+
+struct system_ctx_s {
 	sr_session_ctx_t *startup_session;
-} system_ctx_t;
+};
 
 #endif // SYSTEM_PLUGIN_CONTEXT_H
