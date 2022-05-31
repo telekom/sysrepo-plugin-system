e5775983659d135578e648c458f86dc614eed508
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-05-31 14:31:09 +0200

    system-plugin: fix header guard for startup/load.h

--- src/startup/load.h
+++ src/startup/load.h
@@ -1,8 +1,8 @@
-#ifndef SYSTEM_PLUGIN_STARTUP_STORE_H
-#define SYSTEM_PLUGIN_STARTUP_STORE_H
+#ifndef SYSTEM_PLUGIN_STARTUP_LOAD_H
+#define SYSTEM_PLUGIN_STARTUP_LOAD_H
 
 #include "context.h"
 
 int system_startup_load_data(system_ctx_t *ctx, sr_session_ctx_t *session);
 
-#endif // SYSTEM_PLUGIN_STARTUP_STORE_H
+#endif // SYSTEM_PLUGIN_STARTUP_LOAD_H
