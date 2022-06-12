c91bd453bfbdfa5551a2b6b234b29923ff659ec8
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-06-12 18:49:12 +0200

    system-plugin: implement NTP store API

--- src/system/api/ntp/store.h
+++ src/system/api/ntp/store.h
@@ -4,6 +4,6 @@
 #include "types.h"
 #include "context.h"
 
-int system_ntp_store_server(system_ctx_t *ctx, const system_ntp_server_element_t *head);
+int system_ntp_store_server(system_ctx_t *ctx, system_ntp_server_element_t *head);
 
 #endif // SYSTEM_PLUGIN_API_NTP_STORE_H
