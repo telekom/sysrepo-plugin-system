26f1d6566b03c2bedbd314d6811305c03257f6a3
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-06-15 21:40:52 +0200

    system-plugin: disable authentication store API

--- src/system/api/authentication/store.c
+++ src/system/api/authentication/store.c
@@ -3,12 +3,12 @@
 
 int system_authentication_store_user(system_ctx_t *ctx, system_local_user_element_t *head)
 {
-	int error = 0;
+	int error = -1;
 	return error;
 }
 
 int system_authentication_store_user_authorized_key(system_ctx_t *ctx, const char *user, system_authorized_key_element_t *head)
 {
-	int error = 0;
+	int error = -1;
 	return error;
 }
