bcb508756bacbfedb9c701b4715563e12ac352fc
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-07-21 19:28:00 +0200

    system-plugin: check for ntp feature on startup store

--- src/startup/store.c
+++ src/startup/store.c
@@ -291,151 +291,164 @@ static int system_startup_store_ntp(void *priv, const struct lyd_node *system_co
 	struct lyd_node *udp_container_node = NULL;
 	system_ntp_server_element_t *ntp_server_head = NULL;
 
+	bool ntp_enabled = false;
+	bool ntp_udp_port_enabled = false;
+
 	system_ntp_server_t temp_server = {0};
 	srpc_check_status_t server_check_status = srpc_check_status_none;
 
-	SRPLG_LOG_INF(PLUGIN_NAME, "Storing NTP startup data");
+	SRPC_SAFE_CALL(srpc_check_feature_status(ctx->startup_session, "ietf-system", "ntp", &ntp_enabled), error_out);
+	SRPC_SAFE_CALL(srpc_check_feature_status(ctx->startup_session, "ietf-system", "ntp-udp-port", &ntp_udp_port_enabled), error_out);
 
-	ntp_container_node = srpc_ly_tree_get_child_container(system_container_node, "ntp");
-	if (ntp_container_node) {
-		server_list_node = srpc_ly_tree_get_child_list(ntp_container_node, "server");
+	if (ntp_enabled) {
+		SRPLG_LOG_INF(PLUGIN_NAME, "Storing NTP startup data");
 
-		if (server_list_node) {
-			while (server_list_node) {
-				// process server list node
-				system_ntp_server_init(&temp_server);
+		ntp_container_node = srpc_ly_tree_get_child_container(system_container_node, "ntp");
+		if (ntp_container_node) {
+			server_list_node = srpc_ly_tree_get_child_list(ntp_container_node, "server");
 
-				server_name_leaf_node = srpc_ly_tree_get_child_leaf(server_list_node, "name");
-				udp_container_node = srpc_ly_tree_get_child_container(server_list_node, "udp");
-				server_association_type_leaf_node = srpc_ly_tree_get_child_leaf(server_list_node, "association-type");
-				server_iburst_leaf_node = srpc_ly_tree_get_child_leaf(server_list_node, "iburst");
-				server_prefer_leaf_node = srpc_ly_tree_get_child_leaf(server_list_node, "prefer");
+			if (server_list_node) {
+				while (server_list_node) {
+					// process server list node
+					system_ntp_server_init(&temp_server);
 
-				const char *name = lyd_get_value(server_name_leaf_node);
+					server_name_leaf_node = srpc_ly_tree_get_child_leaf(server_list_node, "name");
+					udp_container_node = srpc_ly_tree_get_child_container(server_list_node, "udp");
+					server_association_type_leaf_node = srpc_ly_tree_get_child_leaf(server_list_node, "association-type");
+					server_iburst_leaf_node = srpc_ly_tree_get_child_leaf(server_list_node, "iburst");
+					server_prefer_leaf_node = srpc_ly_tree_get_child_leaf(server_list_node, "prefer");
 
-				// set name
-				system_ntp_server_set_name(&temp_server, name);
+					const char *name = lyd_get_value(server_name_leaf_node);
 
-				if (!udp_container_node) {
-					SRPLG_LOG_ERR(PLUGIN_NAME, "srpc_ly_tree_get_child_container() failed for udp");
+					// set name
+					system_ntp_server_set_name(&temp_server, name);
+
+					if (!udp_container_node) {
+						SRPLG_LOG_ERR(PLUGIN_NAME, "srpc_ly_tree_get_child_container() failed for udp");
+						goto error_out;
+					}
+
+					server_address_leaf_node = srpc_ly_tree_get_child_leaf(udp_container_node, "address");
+
+					// address
+					if (server_address_leaf_node) {
+						const char *address = lyd_get_value(server_address_leaf_node);
+
+						// set address
+						error = system_ntp_server_set_address(&temp_server, address);
+						if (error) {
+							SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_server_set_address() error (%d)", error);
+							goto error_out;
+						}
+					} else {
+						// no address node -> unable to continue
+						SRPLG_LOG_INF(PLUGIN_NAME, "srpc_ly_tree_get_child_leaf() failed for leaf address");
+						goto error_out;
+					}
+
+					if (ntp_udp_port_enabled) {
+						server_port_leaf_node = srpc_ly_tree_get_child_leaf(udp_container_node, "port");
+
+						// port
+						if (server_port_leaf_node) {
+							const char *port = lyd_get_value(server_port_leaf_node);
+
+							// set port
+							error = system_ntp_server_set_port(&temp_server, port);
+							if (error) {
+								SRPLG_LOG_INF(PLUGIN_NAME, "system_ntp_server_set_port() error (%d)", error);
+								goto error_out;
+							}
+						}
+					}
+
+					// association-type
+					if (server_association_type_leaf_node) {
+						const char *association_type = lyd_get_value(server_association_type_leaf_node);
+
+						error = system_ntp_server_set_association_type(&temp_server, association_type);
+						if (error) {
+							SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_server_set_association_type() error (%d)", error);
+							goto error_out;
+						}
+					} else {
+						// unable to create config entry without association type
+						SRPLG_LOG_ERR(PLUGIN_NAME, "srpc_ly_tree_get_child_leaf() failed for leaf association-type");
+						goto error_out;
+					}
+
+					// iburst
+					if (server_iburst_leaf_node) {
+						const char *iburst = lyd_get_value(server_iburst_leaf_node);
+
+						error = system_ntp_server_set_iburst(&temp_server, iburst);
+						if (error) {
+							SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_server_set_iburst() error (%d)", error);
+							goto error_out;
+						}
+					}
+
+					// prefer
+					if (server_prefer_leaf_node) {
+						const char *prefer = lyd_get_value(server_prefer_leaf_node);
+
+						error = system_ntp_server_set_prefer(&temp_server, prefer);
+						if (error) {
+							SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_server_set_prefer() error (%d)", error);
+							goto error_out;
+						}
+					}
+
+					// append to the list
+					error = system_ntp_server_list_add(&ntp_server_head, temp_server);
+					if (error) {
+						SRPLG_LOG_INF(PLUGIN_NAME, "system_ntp_server_list_add() error (%d)", error);
+						goto error_out;
+					}
+
+					system_ntp_server_free(&temp_server);
+
+					// iterate list
+					server_list_node = srpc_ly_tree_get_list_next(server_list_node);
+				}
+			}
+
+			SRPLG_LOG_INF(PLUGIN_NAME, "Checking NTP server list status on the system");
+
+			server_check_status = system_ntp_check_server(ctx, ntp_server_head);
+
+			SRPLG_LOG_INF(PLUGIN_NAME, "Recieved check status: %d", server_check_status);
+
+			switch (server_check_status) {
+				case srpc_check_status_none:
+					SRPLG_LOG_ERR(PLUGIN_NAME, "Error occured while checking NTP server system values");
 					goto error_out;
-				}
-
-				server_address_leaf_node = srpc_ly_tree_get_child_leaf(udp_container_node, "address");
-				server_port_leaf_node = srpc_ly_tree_get_child_leaf(udp_container_node, "port");
-
-				// address
-				if (server_address_leaf_node) {
-					const char *address = lyd_get_value(server_address_leaf_node);
-
-					// set address
-					error = system_ntp_server_set_address(&temp_server, address);
-					if (error) {
-						SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_server_set_address() error (%d)", error);
-						goto error_out;
-					}
-				} else {
-					// no address node -> unable to continue
-					SRPLG_LOG_INF(PLUGIN_NAME, "srpc_ly_tree_get_child_leaf() failed for leaf address");
+					break;
+				case srpc_check_status_error:
+					SRPLG_LOG_ERR(PLUGIN_NAME, "Error occured while checking NTP server system values");
 					goto error_out;
-				}
+					break;
+				case srpc_check_status_non_existant:
+					SRPLG_LOG_INF(PLUGIN_NAME, "NTP server list values don\'t exist on the system - applying values");
 
-				// port
-				if (server_port_leaf_node) {
-					const char *port = lyd_get_value(server_port_leaf_node);
-
-					// set port
-					error = system_ntp_server_set_port(&temp_server, port);
+					error = system_ntp_store_server(ctx, ntp_server_head);
 					if (error) {
-						SRPLG_LOG_INF(PLUGIN_NAME, "system_ntp_server_set_port() error (%d)", error);
+						SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_store_server() error (%d)", error);
 						goto error_out;
 					}
-				}
 
-				// association-type
-				if (server_association_type_leaf_node) {
-					const char *association_type = lyd_get_value(server_association_type_leaf_node);
-
-					error = system_ntp_server_set_association_type(&temp_server, association_type);
-					if (error) {
-						SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_server_set_association_type() error (%d)", error);
-						goto error_out;
-					}
-				} else {
-					// unable to create config entry without association type
-					SRPLG_LOG_ERR(PLUGIN_NAME, "srpc_ly_tree_get_child_leaf() failed for leaf association-type");
-					goto error_out;
-				}
-
-				// iburst
-				if (server_iburst_leaf_node) {
-					const char *iburst = lyd_get_value(server_iburst_leaf_node);
-
-					error = system_ntp_server_set_iburst(&temp_server, iburst);
-					if (error) {
-						SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_server_set_iburst() error (%d)", error);
-						goto error_out;
-					}
-				}
-
-				// prefer
-				if (server_prefer_leaf_node) {
-					const char *prefer = lyd_get_value(server_prefer_leaf_node);
-
-					error = system_ntp_server_set_prefer(&temp_server, prefer);
-					if (error) {
-						SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_server_set_prefer() error (%d)", error);
-						goto error_out;
-					}
-				}
-
-				// append to the list
-				error = system_ntp_server_list_add(&ntp_server_head, temp_server);
-				if (error) {
-					SRPLG_LOG_INF(PLUGIN_NAME, "system_ntp_server_list_add() error (%d)", error);
-					goto error_out;
-				}
-
-				system_ntp_server_free(&temp_server);
-
-				// iterate list
-				server_list_node = srpc_ly_tree_get_list_next(server_list_node);
+					SRPLG_LOG_INF(PLUGIN_NAME, "Applied NTP server startup values to the system");
+					break;
+				case srpc_check_status_equal:
+					SRPLG_LOG_INF(PLUGIN_NAME, "NTP server startup values already exist on the system - no need to apply anything");
+					break;
+				case srpc_check_status_partial:
+					// TODO: implement
+					break;
 			}
 		}
-
-		SRPLG_LOG_INF(PLUGIN_NAME, "Checking NTP server list status on the system");
-
-		server_check_status = system_ntp_check_server(ctx, ntp_server_head);
-
-		SRPLG_LOG_INF(PLUGIN_NAME, "Recieved check status: %d", server_check_status);
-
-		switch (server_check_status) {
-			case srpc_check_status_none:
-				SRPLG_LOG_ERR(PLUGIN_NAME, "Error occured while checking NTP server system values");
-				goto error_out;
-				break;
-			case srpc_check_status_error:
-				SRPLG_LOG_ERR(PLUGIN_NAME, "Error occured while checking NTP server system values");
-				goto error_out;
-				break;
-			case srpc_check_status_non_existant:
-				SRPLG_LOG_INF(PLUGIN_NAME, "NTP server list values don\'t exist on the system - applying values");
-
-				error = system_ntp_store_server(ctx, ntp_server_head);
-				if (error) {
-					SRPLG_LOG_ERR(PLUGIN_NAME, "system_ntp_store_server() error (%d)", error);
-					goto error_out;
-				}
-
-				SRPLG_LOG_INF(PLUGIN_NAME, "Applied NTP server startup values to the system");
-				break;
-			case srpc_check_status_equal:
-				SRPLG_LOG_INF(PLUGIN_NAME, "NTP server startup values already exist on the system - no need to apply anything");
-				break;
-			case srpc_check_status_partial:
-				// TODO: implement
-				break;
-		}
+	} else {
+		SRPLG_LOG_INF(PLUGIN_NAME, "\"ntp\" feature disabled - skipping NTP startup configuration");
 	}
 
 	goto out;
