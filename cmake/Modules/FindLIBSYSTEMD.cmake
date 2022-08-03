627b1c7efdbf6168b88a2d8aa7e8270ec2937dc1
andrej <you@example.com>
2022-08-03 12:25:41 +0200

    cmake: change to a more exhaustive FindLIBSYSTEMD

--- cmake/Modules/FindLIBSYSTEMD.cmake
+++ cmake/Modules/FindLIBSYSTEMD.cmake
@@ -1,24 +1,46 @@
-# - Try to find SYSTEMD
-# Once done, this will define
+# source: https://github.com/FreeRDP/FreeRDP
+# Module defines
+#  LIBSYSTEMD_FOUND - libsystemd libraries and includes found
+#  LIBSYSTEMD_INCLUDE_DIRS - the libsystemd include directories
+#  LIBSYSTEMD_LIBRARIES - the libsystemd libraries
 #
-#  SYSTEMD_FOUND - system has SYSTEMD
-#  SYSTEMD_INCLUDE_DIRS - the SYSTEMD include directories
-#  SYSTEMD_LIBRARIES - the SYSTEMD library
-find_package(PkgConfig)
+# Cache entries:
+#   LIBSYSTEMD_LIBRARY      - detected libsystemd library
+#   LIBSYSTEMD_INCLUDE_DIR   - detected libsystemd include dir(s)
+#
 
-pkg_check_modules(SYSTEMD_PKGCONF libsystemd)
+if(LIBSYSTEMD_INCLUDE_DIR AND LIBSYSTEMD_LIBRARY)
+    # in cache already
+    set(LIBSYSTEMD_FOUND TRUE)
+    set(LIBSYSTEMD_LIBRARIES ${LIBSYSTEMD_LIBRARY})
+    set(LIBSYSTEMD_INCLUDE_DIRS ${LIBSYSTEMD_INCLUDE_DIR})
+else()
 
-find_path(SYSTEMD_INCLUDE_DIRS
-  NAMES systemd/sd-bus.h
-  PATHS ${SYSTEMD_PKGCONF_INCLUDE_DIRS}
-)
+    find_package(PkgConfig)
+	if(PKG_CONFIG_FOUND)
+		pkg_check_modules(_LIBSYSTEMD_PC QUIET "libsystemd")
+	endif(PKG_CONFIG_FOUND)
 
-find_library(SYSTEMD_LIBRARIES
-  NAMES systemd
-  PATHS ${SYSTEMD_PKGCONF_LIBRARY_DIRS}
-)
+	find_path(LIBSYSTEMD_INCLUDE_DIR systemd/sd-journal.h
+			${_LIBSYSTEMD_PC_INCLUDE_DIRS}
+			/usr/include
+			/usr/local/include
+	)
+	mark_as_advanced(LIBSYSTEMD_INCLUDE_DIR)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Systemd DEFAULT_MSG SYSTEMD_INCLUDE_DIRS SYSTEMD_LIBRARIES)
+	find_library (LIBSYSTEMD_LIBRARY NAMES systemd
+			PATHS
+			${_LIBSYSTEMD_PC_LIBDIR}
+    )
+    mark_as_advanced(LIBSYSTEMD_LIBRARY)
 
-mark_as_advanced(SYSTEMD_INCLUDE_DIRS SYSTEMD_LIBRARIES)
+    include(FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(libsystemd DEFAULT_MSG LIBSYSTEMD_LIBRARY LIBSYSTEMD_INCLUDE_DIR)
+
+    if(libsystemd_FOUND)
+        set(LIBSYSTEMD_LIBRARIES ${LIBSYSTEMD_LIBRARY})
+        set(LIBSYSTEMD_INCLUDE_DIRS ${LIBSYSTEMD_INCLUDE_DIR})
+    endif()
+
+endif()
+
