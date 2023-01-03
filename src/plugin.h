d1e7fb544755c098ecbf53e8713c75e1bdb67be6
Mateo Cindric <mateocindricdev@gmail.com>
2023-01-03 14:36:01 +0000

    system-plugin: fix header include guards

--- src/plugin.h
+++ src/plugin.h
@@ -18,4 +18,4 @@
 int sr_plugin_init_cb(sr_session_ctx_t *session, void **private_data);
 void sr_plugin_cleanup_cb(sr_session_ctx_t *session, void *private_data);
 
-#endif // BRIDGING_PLUGIN_H
+#endif // SYSTEM_PLUGIN_H
