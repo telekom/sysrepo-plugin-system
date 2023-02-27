fd5feb8a7e495c97a2cf35fd95c6816aeba3831c
Mateo Cindric <mateocindricdev@gmail.com>
2023-02-27 15:52:49 +0000

    ietf-system-plugin: implement platform machine oper get functor

--- src/core/sub/oper.cpp
+++ src/core/sub/oper.cpp
@@ -146,6 +146,15 @@ namespace sub::oper {
         std::optional<ly::DataNode>& output)
     {
         sr::ErrorCode error = sr::ErrorCode::Ok;
+
+        struct utsname uname_data = { 0 };
+
+        if (uname(&uname_data) < 0) {
+            return sr::ErrorCode::Internal;
+        }
+
+        output->newPath("machine", uname_data.machine);
+
         return error;
     }
 
