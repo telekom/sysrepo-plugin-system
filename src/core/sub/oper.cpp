a0330f59c307f8a1b3ebff0a436aa7d61fd02b12
Mateo Cindric <mateocindricdev@gmail.com>
2023-02-27 15:51:22 +0000

    ietf-system-plugin: implement os-release oper get functor

--- src/core/sub/oper.cpp
+++ src/core/sub/oper.cpp
@@ -70,6 +70,15 @@ namespace sub::oper {
         std::optional<ly::DataNode>& output)
     {
         sr::ErrorCode error = sr::ErrorCode::Ok;
+
+        struct utsname uname_data = { 0 };
+
+        if (uname(&uname_data) < 0) {
+            return sr::ErrorCode::Internal;
+        }
+
+        output->newPath("os-release", uname_data.release);
+
         return error;
     }
 
