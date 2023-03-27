d5379140be4f43cc569a504a1e6deec4a2422e4d
Mateo Cindric <mateocindricdev@gmail.com>
2023-03-27 11:24:49 +0000

    ietf-system-plugin: remove unused includes

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -5,9 +5,6 @@
 #include "core/sub/change.hpp"
 #include "core/sub/oper.hpp"
 #include "core/sub/rpc.hpp"
-#include "sysrepo-cpp/Enum.hpp"
-#include "sysrepo-cpp/Subscription.hpp"
-#include "sysrepo.h"
 
 #include <sysrepo-cpp/Session.hpp>
 #include <sysrepo-cpp/utils/utils.hpp>
