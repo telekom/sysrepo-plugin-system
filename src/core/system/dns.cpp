c12022afe5cc8f556eca42cb769ac1a11d4b1ca1
Mateo Cindric <mateocindricdev@gmail.com>
2023-03-28 10:54:40 +0000

    dns-resolver: add DNS types

---
+++ src/core/system/dns.cpp
@@ -0,0 +1,13 @@
+#include "dns.hpp"
+
+namespace ietf::sys::dns {
+
+/**
+ * @brief Default constructor.
+ */
+DnsServer::DnsServer()
+    : Port(53)
+{
+}
+
+}
---
+++ src/core/system/dns.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+namespace ietf::sys::dns {
+
+/**
+ * @brief DNS server.
+ */
+struct DnsServer {
+    std::string Name; ///< Arbitrary name of the DNS server.
+    std::string Address; ///< Address of the DNS server.
+    std::uint16_t Port; ///< Port of the DNS server.
+
+    using List = std::vector<DnsServer>;
+
+    /**
+     * @brief Default constructor.
+     */
+    DnsServer();
+
+    /**
+     * @brief Get the list of DNS servers.
+     * @return List of DNS servers.
+     */
+    static DnsServer::List getServers();
+};
+
+/**
+ * @brief DNS search element type alias.
+ */
+using DnsSearch = std::string;
+
+/**
+ * @brief DNS search list type alias.
+ */
+using DnsSearchList = std::vector<DnsSearch>;
+
+}
