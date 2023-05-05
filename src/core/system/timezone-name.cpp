57b9e8a7c1d1b8b0e83b341220ca1d85bf81de64
pikimk <pikimk@hotmail.com>
2023-05-05 13:52:28 +0200

    CREATED: sdbus interface CHANGED: using sdbus for changing timezone name

--- src/core/system/timezone-name.cpp
+++ src/core/system/timezone-name.cpp
@@ -1,55 +1,12 @@
 #include "timezone-name.hpp"
 
-#include <core/common.hpp>
-
-
 namespace ietf::sys {
-template <class GET, class ...SET>
-SdBUS<GET, SET...>::SdBUS(std::string destination, std::string objectPath, std::string interface, std::string setmethodname, std::string getmethodname)
-    : dest { destination }
-    , objPath { objectPath }
-    , interface {
-    interface
-}, sdbusSetMethod { setmethodname }, sdbusGetMethod { getmethodname } {};
-
-template <class GET, class ...SET> bool SdBUS<GET, SET...>::exportToSdBus(SET ...data)
-{
-    bool error = false;
-
-    try {
-        auto proxy = sdbus::createProxy(this->dest, this->objPath);
-        proxy->callMethod(this->sdbusSetMethod).onInterface(this->interface).withArguments(data...);
-    } catch (sdbus::Error& e) {
-        throw std::runtime_error(e.getMessage());
-        error = true;
-    };
-
-    return error;
-};
-
-template <class GET, class ...SET> GET SdBUS<GET, SET...>::importFromSdBus()
-{
-    GET data;
-
-    try {
-        auto proxy = sdbus::createProxy(this->dest, this->objPath);
-        sdbus::Variant v = proxy->getProperty(this->sdbusGetMethod).onInterface(this->interface);
-        data = v.get<GET>();
-    } catch (sdbus::Error& e) {
-        throw std::runtime_error(e.getMessage());
-    }
-    return data;
-}
 
 Timezone::Timezone()
     : SdBUS<std::string, std::string, bool>(
         "org.freedesktop.timedate1", "/org/freedesktop/timedate1", "org.freedesktop.timedate1", "SetTimezone", "Timezone") {};
 
-bool Timezone::setTimezone(std::string timezone)
-{
-
-    return !exportToSdBus(timezone,true);
-}
+bool Timezone::setTimezone(std::string timezone) { return !exportToSdBus(timezone, true); }
 
 std::string Timezone::getTimezone()
 {
@@ -57,4 +14,5 @@ std::string Timezone::getTimezone()
     std::string data = importFromSdBus();
     return data;
 }
+
 }
--- src/core/system/timezone-name.hpp
+++ src/core/system/timezone-name.hpp
@@ -1,25 +1,11 @@
 #pragma once
 
-#include <string>
-#include <sdbus-c++/sdbus-c++.h>
+#include <core/sdbus.hpp>
 
 namespace ietf::sys {
 
-// set and get types
-template <class GET, class ...SET> class SdBUS {
-public:
-    SdBUS(std::string destination, std::string objectPath, std::string interface, std::string setmethodname, std::string getmethodname);
-
-protected:
-    bool exportToSdBus(SET ...data);
-    GET importFromSdBus();
-
-private:
-    std::string dest, objPath, interface, sdbusSetMethod, sdbusGetMethod;
-};
-
 // it sends a string and a boolean, receives string
-class Timezone : public SdBUS<std::string , std::string, bool> {
+class Timezone : public SdBUS<std::string, std::string, bool> {
 public:
     Timezone();
 
