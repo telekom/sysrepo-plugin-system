1fac1a6c890f421c8b54c6df10b437a20ef9d369
Mateo Cindrić <mateocindricdev@gmail.com>
2023-06-06 14:25:47 +0200

    refactor(auth): use separate callbacks for name and password changes

--- src/core/sub/change.cpp
+++ src/core/sub/change.cpp
@@ -529,6 +529,122 @@ namespace sub::change {
      * @param ctx Plugin module change context.
      *
      */
+    AuthUserNameModuleChangeCb::AuthUserNameModuleChangeCb(std::shared_ptr<ietf::sys::ModuleChangeContext> ctx) { m_ctx = ctx; }
+
+    /**
+     * sysrepo-plugin-generator: Generated module change operator() for path /ietf-system:system/authentication/user[name='%s']/name.
+     *
+     * @param session An implicit session for the callback.
+     * @param subscriptionId ID the subscription associated with the callback.
+     * @param moduleName The module name used for subscribing.
+     * @param subXPath The optional xpath used at the time of subscription.
+     * @param event Type of the event that has occured.
+     * @param requestId Request ID unique for the specific module_name. Connected events for one request (SR_EV_CHANGE and
+     * SR_EV_DONE, for example) have the same request ID.
+     *
+     * @return Error code.
+     *
+     */
+    sr::ErrorCode AuthUserNameModuleChangeCb::operator()(sr::Session session, uint32_t subscriptionId, std::string_view moduleName,
+        std::optional<std::string_view> subXPath, sr::Event event, uint32_t requestId)
+    {
+        sr::ErrorCode error = sr::ErrorCode::Ok;
+
+        switch (event) {
+        case sysrepo::Event::Change:
+            for (auto& change : session.getChanges(subXPath->data())) {
+                SRPLG_LOG_DBG(ietf::sys::PLUGIN_NAME, "Value of %s modified.", change.node.path().c_str());
+                SRPLG_LOG_DBG(ietf::sys::PLUGIN_NAME, "Value of %s modified.", change.node.schema().name().data());
+
+                SRPLG_LOG_DBG(
+                    ietf::sys::PLUGIN_NAME, "\n%s", change.node.printStr(libyang::DataFormat::XML, libyang::PrintFlags::WithDefaultsAll)->data());
+
+                switch (change.operation) {
+                case sysrepo::ChangeOperation::Created:
+                    // create user
+                    break;
+                case sysrepo::ChangeOperation::Modified:
+                    // should not be possible to edit name
+                    break;
+                case sysrepo::ChangeOperation::Deleted:
+                    // delete user
+                    break;
+                case sysrepo::ChangeOperation::Moved:
+                    break;
+                }
+            }
+            break;
+        default:
+            break;
+        }
+
+        return sr::ErrorCode::CallbackFailed;
+    }
+
+    /**
+     * sysrepo-plugin-generator: Generated default constructor.
+     *
+     * @param ctx Plugin module change context.
+     *
+     */
+    AuthUserPasswordModuleChangeCb::AuthUserPasswordModuleChangeCb(std::shared_ptr<ietf::sys::ModuleChangeContext> ctx) { m_ctx = ctx; }
+
+    /**
+     * sysrepo-plugin-generator: Generated module change operator() for path /ietf-system:system/authentication/user[name='%s']/password.
+     *
+     * @param session An implicit session for the callback.
+     * @param subscriptionId ID the subscription associated with the callback.
+     * @param moduleName The module name used for subscribing.
+     * @param subXPath The optional xpath used at the time of subscription.
+     * @param event Type of the event that has occured.
+     * @param requestId Request ID unique for the specific module_name. Connected events for one request (SR_EV_CHANGE and
+     * SR_EV_DONE, for example) have the same request ID.
+     *
+     * @return Error code.
+     *
+     */
+    sr::ErrorCode AuthUserPasswordModuleChangeCb::operator()(sr::Session session, uint32_t subscriptionId, std::string_view moduleName,
+        std::optional<std::string_view> subXPath, sr::Event event, uint32_t requestId)
+    {
+        sr::ErrorCode error = sr::ErrorCode::Ok;
+
+        switch (event) {
+        case sysrepo::Event::Change:
+            for (auto& change : session.getChanges(subXPath->data())) {
+                SRPLG_LOG_DBG(ietf::sys::PLUGIN_NAME, "Value of %s modified.", change.node.path().c_str());
+                SRPLG_LOG_DBG(ietf::sys::PLUGIN_NAME, "Value of %s modified.", change.node.schema().name().data());
+
+                SRPLG_LOG_DBG(
+                    ietf::sys::PLUGIN_NAME, "\n%s", change.node.printStr(libyang::DataFormat::XML, libyang::PrintFlags::WithDefaultsAll)->data());
+
+                switch (change.operation) {
+                case sysrepo::ChangeOperation::Created:
+                    // create user password
+                    break;
+                case sysrepo::ChangeOperation::Modified:
+                    // modify user password
+                    break;
+                case sysrepo::ChangeOperation::Deleted:
+                    // delete user or delete user password
+                    break;
+                case sysrepo::ChangeOperation::Moved:
+                    break;
+                }
+            }
+            break;
+        default:
+            break;
+        }
+
+        return sr::ErrorCode::CallbackFailed;
+    }
+
+    /**
+     * sysrepo-plugin-generator: Generated default constructor.
+     *
+     * @param ctx Plugin module change context.
+     *
+     */
     AuthUserModuleChangeCb::AuthUserModuleChangeCb(std::shared_ptr<ietf::sys::ModuleChangeContext> ctx) { m_ctx = ctx; }
 
     /**
@@ -553,48 +669,49 @@ namespace sub::change {
         switch (event) {
         case sysrepo::Event::Change:
             for (auto& change : session.getChanges(subXPath->data())) {
+                SRPLG_LOG_DBG(ietf::sys::PLUGIN_NAME, "Value of %s modified.", change.node.path().c_str());
                 SRPLG_LOG_DBG(ietf::sys::PLUGIN_NAME, "Value of %s modified.", change.node.schema().name().data());
 
                 SRPLG_LOG_DBG(
                     ietf::sys::PLUGIN_NAME, "\n%s", change.node.printStr(libyang::DataFormat::XML, libyang::PrintFlags::WithDefaultsAll)->data());
 
-                const auto& name_node = change.node.findPath("name");
-                const auto& password_node = change.node.findPath("password");
+                // const auto& name_node = change.node.findPath("name");
+                // const auto& password_node = change.node.findPath("password");
 
-                const auto& user_meta = ietf::sys::getMetaValuesHash(change.node.meta());
-                const auto& name_meta = ietf::sys::getMetaValuesHash(name_node->meta());
-                auto password_meta = std::map<std::string, std::string>();
+                // const auto& user_meta = ietf::sys::getMetaValuesHash(change.node.meta());
+                // const auto& name_meta = ietf::sys::getMetaValuesHash(name_node->meta());
+                // auto password_meta = std::map<std::string, std::string>();
 
-                const auto& name_value = name_node->asTerm().value();
-                const auto& name = std::get<std::string>(name_value);
-                auto password = std::optional<std::string>(std::nullopt);
+                // const auto& name_value = name_node->asTerm().value();
+                // const auto& name = std::get<std::string>(name_value);
+                // auto password = std::optional<std::string>(std::nullopt);
 
-                if (password_node) {
-                    const auto& password_value = password_node->asTerm().value();
-                    password = std::get<std::string>(password_value);
-                    password_meta = ietf::sys::getMetaValuesHash(password_node->meta());
-                }
+                // if (password_node) {
+                //     const auto& password_value = password_node->asTerm().value();
+                //     password = std::get<std::string>(password_value);
+                //     password_meta = ietf::sys::getMetaValuesHash(password_node->meta());
+                // }
 
-                switch (change.operation) {
-                case sysrepo::ChangeOperation::Created: {
-                    // create new user or create new password
-                    const auto& name_operation = name_meta.find("operation");
-                    if (name_operation != name_meta.end()) {
-                        // name has operation
-                    } else {
-                        // no name operation - only password change
-                    }
-                    break;
-                }
-                case sysrepo::ChangeOperation::Modified:
-                    // modify user password
-                    break;
-                case sysrepo::ChangeOperation::Deleted:
-                    // delete user or delete user password
-                    break;
-                case sysrepo::ChangeOperation::Moved:
-                    break;
-                }
+                // switch (change.operation) {
+                // case sysrepo::ChangeOperation::Created: {
+                //     // create new user or create new password
+                //     const auto& name_operation = name_meta.find("operation");
+                //     if (name_operation != name_meta.end()) {
+                //         // name has operation
+                //     } else {
+                //         // no name operation - only password change
+                //     }
+                //     break;
+                // }
+                // case sysrepo::ChangeOperation::Modified:
+                //     // modify user password
+                //     break;
+                // case sysrepo::ChangeOperation::Deleted:
+                //     // delete user or delete user password
+                //     break;
+                // case sysrepo::ChangeOperation::Moved:
+                //     break;
+                // }
             }
             break;
         default:
--- src/core/sub/change.hpp
+++ src/core/sub/change.hpp
@@ -523,6 +523,74 @@ namespace sub::change {
     };
 
     /**
+     * @brief sysrepo-plugin-generator: Generated module change functor for path /ietf-system:system/authentication/user[name='%s']/name.
+     */
+    class AuthUserNameModuleChangeCb {
+    public:
+        /**
+         * sysrepo-plugin-generator: Generated default constructor.
+         *
+         * @param ctx Plugin module change context.
+         *
+         */
+        AuthUserNameModuleChangeCb(std::shared_ptr<ietf::sys::ModuleChangeContext> ctx);
+
+        /**
+         * sysrepo-plugin-generator: Generated module change operator() for path /ietf-system:system/authentication/user[name='%s'].
+         *
+         * @param session An implicit session for the callback.
+         * @param subscriptionId ID the subscription associated with the callback.
+         * @param moduleName The module name used for subscribing.
+         * @param subXPath The optional xpath used at the time of subscription.
+         * @param event Type of the event that has occured.
+         * @param requestId Request ID unique for the specific module_name. Connected events for one request (SR_EV_CHANGE and
+         * SR_EV_DONE, for example) have the same request ID.
+         *
+         * @return Error code.
+         *
+         */
+        sr::ErrorCode operator()(sr::Session session, uint32_t subscriptionId, std::string_view moduleName, std::optional<std::string_view> subXPath,
+            sr::Event event, uint32_t requestId);
+
+    private:
+        std::shared_ptr<ietf::sys::ModuleChangeContext> m_ctx;
+    };
+
+    /**
+     * @brief sysrepo-plugin-generator: Generated module change functor for path /ietf-system:system/authentication/user[name='%s']/password.
+     */
+    class AuthUserPasswordModuleChangeCb {
+    public:
+        /**
+         * sysrepo-plugin-generator: Generated default constructor.
+         *
+         * @param ctx Plugin module change context.
+         *
+         */
+        AuthUserPasswordModuleChangeCb(std::shared_ptr<ietf::sys::ModuleChangeContext> ctx);
+
+        /**
+         * sysrepo-plugin-generator: Generated module change operator() for path /ietf-system:system/authentication/user[name='%s'].
+         *
+         * @param session An implicit session for the callback.
+         * @param subscriptionId ID the subscription associated with the callback.
+         * @param moduleName The module name used for subscribing.
+         * @param subXPath The optional xpath used at the time of subscription.
+         * @param event Type of the event that has occured.
+         * @param requestId Request ID unique for the specific module_name. Connected events for one request (SR_EV_CHANGE and
+         * SR_EV_DONE, for example) have the same request ID.
+         *
+         * @return Error code.
+         *
+         */
+        sr::ErrorCode operator()(sr::Session session, uint32_t subscriptionId, std::string_view moduleName, std::optional<std::string_view> subXPath,
+            sr::Event event, uint32_t requestId);
+
+    private:
+        std::shared_ptr<ietf::sys::ModuleChangeContext> m_ctx;
+    };
+
+    /**
      * @brief sysrepo-plugin-generator: Generated module change functor for path /ietf-system:system/authentication/user[name='%s'].
      */
     class AuthUserModuleChangeCb {
