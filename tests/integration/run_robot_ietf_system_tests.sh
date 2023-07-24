6b09fc117fa1eacac01f449dcc01c3bf15f40c7d
andreiosg <andrej.gardijan@protonmail.com>
2023-07-24 10:11:39 +0200

    feat(integration-tests): improve deps check, add exit codes

--- tests/integration/run_robot_ietf_system_tests.sh
+++ tests/integration/run_robot_ietf_system_tests.sh
@@ -1,25 +1,38 @@
 #!/bin/env bash
 
+EXIT_SUCCESS=0
+EXIT_NO_BINARY=1
+EXIT_NO_MODULES=2
+
 log() { printf ">>> %s\n" "$*" >&2; }
 
+# check if path to the system plugin binary is set
 if [ -z $SYSREPO_SYSTEM_PLUGIN_PATH ]; then
     log "SYSREPO_SYSTEM_PLUGIN_PATH not set to the plugin executable path"
-    exit 1
+    exit $EXIT_NO_BINARY
 fi
 
 log "check if required modules are installed"
 
-fail_module_check() { log "modules check failed - missing a module" && exit 1; }
-
-python3 -m pip show rpaframework &> /dev/null || fail_module_check
-python3 -m pip show SysrepoLibrary &> /dev/null || fail_module_check
+# check dependencies
+DEPS=("rpaframework" \
+      "robotframework-sysrepolibrary")
+for MODULE in "${DEPS[@]}"; do
+    python3 -m pip show $MODULE &> /dev/null
+    if [ $? -ne 0 ]; then
+        log "module $MODULE not found" 
+        exit $EXIT_NO_MODULES
+    fi
+done
 
 log "modules check passed"
 
+# run the tests
 robot                      \
     --output output.xml    \
     robot-ietf-system
 
+# rerun based on exit code
 if [ $? -ne 0 ]; then
     log "some tests failed, rerunning tests"
     robot                         \
@@ -33,4 +46,4 @@ if [ $? -ne 0 ]; then
         --merge output.xml output_rerun.xml 
 fi
 
-exit 0
+exit $EXIT_SUCCESS
